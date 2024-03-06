f0da0fb02f7313041c9d378c58dd0a36a2ff9082
AnshumanRoy <anshumanroy1707@gmail.com>
2024-03-06 11:08:14 +0530

    Added question answering feature

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+from langchain.chains import (
+    ConversationalRetrievalChain,
+)
+
 import os
 
 text_splitter = RecursiveCharacterTextSplitter(
@@ -34,7 +39,30 @@ def reply_whatsapp():
         if not os.path.isdir("chroma"):
             msg = response.message("No context provided. Please send a PDF file to query.")
         else:
-            msg = response.message("This is the response to your query.")
+            query = request.values.get('Body')
+            vectordb = Chroma(persist_directory='chroma', embedding_function=embeddings)
+            retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k": 1})
+
+            message_history = ChatMessageHistory()
+
+            memory = ConversationBufferMemory(
+                memory_key="chat_history",
+                output_key="answer",
+                chat_memory=message_history,
+                return_messages=True,
+            )
+
+            chain = ConversationalRetrievalChain.from_llm(
+                ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True),
+                chain_type="stuff",
+                retriever=retriever,
+                memory=memory,
+                return_source_documents=True,
+            )
+
+            result = chain.invoke(query)["answer"]
+
+            msg = response.message(result)
     else:
         if request.values.get("MediaContentType0") == "application/pdf":
             loader = PyPDFLoader(file_uri)
